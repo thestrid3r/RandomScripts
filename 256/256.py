273c7e803b0dd42fa9bed9d974d874b9c46e5eed
Azrael <azrael@localhost.localdomain>
2015-11-12 22:11:36 +0530

    Python scripts to check if your X-term supports 256 colors or not

--- 256/256.py
+++ 256/256.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2006 by Johannes Zellner, <johannes@zellner.org>
 # modified by mac@calmar.ws to fit my output needs
 # modified by crncosta@carloscosta.org to fit my output needs
-# modifed by azrael to fit my output needs
+# modifed by azrael to fit my output need :-P
 
 import sys
 import os
